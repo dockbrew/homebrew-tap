36141c332ecc6d576b0950dd9d7c433b6b1e0ebf
Hamdi LAADHARI <killerwolf06@gmail.com>
2015-11-24 23:24:29 +0100

    WIP

--- docker-composer.rb
+++ docker-composer.rb
@@ -6,5 +6,6 @@ class DockerComposer < Formula
   sha256 "f3ba5e73a4f44239558517fa341b81837b9cf9bce97a0d43c505a3178e7289a8"
 
   def install
+    bin.install "docker-composer"
   end
 end
--- firstcli.rb
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo Formal Friday Club rocks!
