038d34e81141f55f2c7c66167a5599f1a846238e
Hamdi LAADHARI <killerwolf06@gmail.com>
2015-11-25 00:57:16 +0100

    adding docker hugo

---
+++ docker-hugo.rb
@@ -0,0 +1,11 @@
+class DockerHugo < Formula
+  desc "install docker"
+  homepage ""
+  url "https://github.com/dockbrew/docker-hugo/archive/0.13.tar.gz"
+  version "0.13"
+  sha256 "1d6aa5e07fed2a58dbe7e066f82b74c554af489d7ea0196682a1871b78cf56c2"
+
+  def install
+    bin.install "docker-hugo"
+  end
+end
