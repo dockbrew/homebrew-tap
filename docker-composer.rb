489703daed504694c14e2966cb0c6c88c0bffef4
Hamdi LAADHARI <killerwolf06@gmail.com>
2015-11-24 23:13:21 +0100

    no message

---
+++ docker-composer.rb
@@ -0,0 +1,10 @@
+class DockerComposer < Formula
+  desc "install docker"
+  homepage ""
+  url "https://github.com/dockbrew/docker-composer/archive/0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "f3ba5e73a4f44239558517fa341b81837b9cf9bce97a0d43c505a3178e7289a8"
+
+  def install
+  end
+end
