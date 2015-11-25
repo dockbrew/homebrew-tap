45c68b563af2919688bfc672f7323c215d6f770e
Hamdi LAADHARI <killerwolf06@gmail.com>
2015-11-25 21:59:06 +0100

    adding homepage of ab formula

--- ab.rb
+++ ab.rb
@@ -1,6 +1,6 @@
 class Ab < Formula
   desc "install ApacheBench as a docker container"
-  homepage ""
+  homepage "https://github.com/jfrazelle/dockerfiles/tree/master/ab"
   url "https://github.com/dockbrew/ab/archive/2.3.tar.gz"
   version "2.3"
   sha256 "8818e940beb458743bdf3a1fc3c3b373956fc89379eb5025ad0f6b8c2465aad6"
