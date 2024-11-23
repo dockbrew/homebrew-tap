85b26a950c759a2fd4de1962ac4cb5a66b503158
Hamdi LAADHARI <killerwolf06@gmail.com>
2024-11-23 18:51:35 +0000

    wip

--- Formula/ab.rb
+++ Formula/ab.rb
@@ -1,22 +1,15 @@
-# Class definition for the ab formula
 class Ab < Formula
-  # Description of the formula
   desc "install ApacheBench as a docker container"
-  # Homepage URL
   homepage "https://github.com/jfrazelle/dockerfiles/tree/master/ab"
-  # URL of the formula source
   url "https://github.com/dockbrew/ab/archive/2.3.tar.gz"
-  # Version number
   version "2.3"
-  # SHA256 checksum for verification
   sha256 "8818e940beb458743bdf3a1fc3c3b373956fc89379eb5025ad0f6b8c2465aad6"
 
-  # Dependency on Docker
   depends_on "docker"
 
-  # Installation method
   def install
     bin.install "ab"
+    system "brew", "test"
   end
 
   test do
